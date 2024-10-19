a38c1ceb2ee4643e9e111162c298649999d2d9c2
pepsirei <Sorrybuthurt@gmail.com>
2024-10-19 22:41:11 +0300

    Changes to be committed: 	modified:   .env.prod 	modified:   android/app/build.gradle 	modified:   android/app/src/main/AndroidManifest.xml 	modified:   lib/main.dart 	modified:   lib/screens/app_screen.dart 	modified:   lib/screens/authscreen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:petrolchik/firebase/firebase_options.dart';
 import 'package:petrolchik/screens/authcheck.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:yandex_maps_mapkit/init.dart' as init;
 
 void main() async {
   await dotenv.load(fileName: ".env.prod");
-
+  await init.initMapkit(apiKey: '28e79d50-feeb-4c1f-9865-d16417683e6d');
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
--- lib/screens/app_screen.dart
+++ lib/screens/app_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:petrolchik/bloc/app_bloc.dart';
-import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:yandex_maps_mapkit/mapkit.dart';
+import 'package:yandex_maps_mapkit/yandex_map.dart';
 
 class AppScreen extends StatefulWidget {
   const AppScreen({super.key});
@@ -11,6 +12,7 @@ class AppScreen extends StatefulWidget {
 }
 
 final _AppBloc = AppBloc();
+MapWindow? _mapWindow;
 
 class _AppScreenState extends State<AppScreen> {
   @override
@@ -20,7 +22,9 @@ class _AppScreenState extends State<AppScreen> {
           bloc: _AppBloc,
           builder: (context, state) {
             if (true) {
-              return Container();
+              return YandexMap(onMapCreated: (mapwindow) {
+                _mapWindow = mapwindow;
+              });
             }
           }),
     );
